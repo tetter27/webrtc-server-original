3d56d19161efcb36c180021c4d17dbc588aaa52f
tetta maeda <tetter@A1003483-tetter.local>
2022-11-11 09:04:57 +0900

    refine

--- userMedia.js
+++ userMedia.js
@@ -5,16 +5,18 @@ const elementTextareaLocalSDP = document.getElementById( "local_sdp_text" );
 const elementTextareaRemoteSDP = document.getElementById( "remote_sdp_text" );
 
 
-// getUserMedia API !!
+// Run when just accessed this page.
 navigator.mediaDevices.getUserMedia({"video": true, /*"audio": true*/})
 .then( ( stream ) =>
 {
+    //Display video from usb camera as the local stream.
     elementVideoLocal.srcObject = stream;
     console.log("Stream has set.");
 
     let config = { "iceServers": [] };
     let rtcPeerConnection = new RTCPeerConnection( config );
 
+    // The OfferSDP is pasted when "ICE gathering state change" becomes "complete".
     setupRTCPeerConnectionEventHandler( rtcPeerConnection );
 
     stream.getTracks().forEach( ( track ) =>
@@ -22,10 +24,8 @@ navigator.mediaDevices.getUserMedia({"video": true, /*"audio": true*/})
             rtcPeerConnection.addTrack( track, stream );
         } );
 
-    // OfferSDPの作成
+    // Create OfferSDP
     createOfferSDP( rtcPeerConnection );
-    
-    
 } )
 .catch( ( error ) =>
 {
@@ -33,7 +33,6 @@ navigator.mediaDevices.getUserMedia({"video": true, /*"audio": true*/})
     alert( "Could not setup." );
     return;
 } );
-
 
 
 
@@ -60,19 +59,19 @@ function createPeerConnection(stream){
 
 function createOfferSDP(rtcPeerConnection)
 {
-    // OfferSDPの作成
+
     console.log( "Call : rtcPeerConnection.createOffer()" );
     rtcPeerConnection.createOffer()
         .then( ( sessionDescription ) =>
         {
-            // 作成されたOfferSDPををLocalDescriptionに設定
+            // Set OfferSDP ot LocalDescription
             console.log( "Call : rtcPeerConnection.setLocalDescription()" );
             return rtcPeerConnection.setLocalDescription( sessionDescription );
         } )
         .then( () =>
         {
-            // Vanilla ICEの場合は、まだSDPを相手に送らない
-            // Trickle ICEの場合は、初期SDPを相手に送る
+            // If Vanilla ICE, SDP is not sending yet.
+            // If Trickle ICE, initial SDP is sending.
         } )
         .catch( ( error ) =>
         {
@@ -80,119 +79,9 @@ function createOfferSDP(rtcPeerConnection)
         } );
 }
 
+// The offer SDP is pasted when "ICE gathering state change" becomes "complete".
+function pastelocalSDPtoTextarea(rtcPeerConnection){
+    
+    elementTextareaLocalSDP.value = rtcPeerConnection.localDescription.sdp;
 
-function setupRTCPeerConnectionEventHandler(rtcPeerConnection){
-
-    // Handler for the "Negotiation needed" event
-    //   This event occurs when the change that needs session negotiation occurs.
-    //   Since some session changes cannot be negotiated as an answer, this negotiation must be performed as an offerer.
-    //   Most generally, the negotiationneeded event occurs after a transmission track is added to the RTCPeerConnection.
-    //   This event does not occur when another negotiation is already ongoing.
-    rtcPeerConnection.onnegotiationneeded = () =>{
-        console.log("Event: Navigation needed.");
-    };
-
-    // Handler for the "ICE Candidate" event
-    //   This event always occurs when any ICE agent starts streaming message.
-    rtcPeerConnection.onicecandidate = ( event ) =>
-    {
-        console.log( "Event : ICE candidate" );
-        if( event.candidate )
-        {   // ICE candidateがある
-            console.log( "- ICE candidate : ", event.candidate );
-
-            // Vanilla ICEの場合は、何もしない
-            // Trickle ICEの場合は、ICE candidateを相手に送る
-        }
-        else
-        {   // ICE candiateがない = ICE candidate の収集終了。
-            console.log( "- ICE candidate : empty" );
-        }
-    };
-
-    // ICE candidate error イベントが発生したときのイベントハンドラ
-    // - このイベントは、ICE候補の収集処理中にエラーが発生した場合に発生する。
-    //   see : https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/onicecandidateerror
-    rtcPeerConnection.onicecandidateerror = ( event ) =>
-    {
-        console.error( "Event : ICE candidate error. error code : ", event.errorCode );
-    };
-
-    // ICE gathering state change イベントが発生したときのイベントハンドラ
-    // - このイベントは、ICE gathering stateが変化したときに発生する。
-    //   言い換えれば、ICEエージェントがアクティブに候補者を収集しているかどうかが変化したときに発生する。
-    //   see : https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/onicegatheringstatechange
-    rtcPeerConnection.onicegatheringstatechange = () =>
-    {
-        console.log( "Event : ICE gathering state change" );
-        console.log( "- ICE gathering state : ", rtcPeerConnection.iceGatheringState );
-
-        if( "complete" === rtcPeerConnection.iceGatheringState )
-        {
-            // Vanilla ICEの場合は、ICE candidateを含んだOfferSDP/AnswerSDPを相手に送る
-            // Trickle ICEの場合は、何もしない
-
-            // Offer側のOfferSDP用のテキストエリアに貼付
-            console.log( "- Set OfferSDP in textarea" );
-            elementTextareaLocalSDP.value = rtcPeerConnection.localDescription.sdp;
-
-        }
-    };
-
-    // ICE connection state change イベントが発生したときのイベントハンドラ
-    // - このイベントは、ネゴシエーションプロセス中にICE connection stateが変化するたびに発生する。 
-    // - 接続が成功すると、通常、状態は「new」から始まり、「checking」を経て、「connected」、最後に「completed」と遷移します。 
-    //   ただし、特定の状況下では、「connected」がスキップされ、「checking」から「completed」に直接移行する場合があります。
-    //   これは、最後にチェックされた候補のみが成功した場合に発生する可能性があり、成功したネゴシエーションが完了する前に、
-    //   収集信号と候補終了信号の両方が発生します。
-    //   see : https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/iceconnectionstatechange_event
-    rtcPeerConnection.oniceconnectionstatechange = () =>
-    {
-        console.log( "Event : ICE connection state change" );
-        console.log( "- ICE connection state : ", rtcPeerConnection.iceConnectionState );
-        // "disconnected" : コンポーネントがまだ接続されていることを確認するために、RTCPeerConnectionオブジェクトの少なくとも
-        //                  1つのコンポーネントに対して失敗したことを確認します。これは、"failed "よりも厳しいテストではなく、
-        //                  断続的に発生し、信頼性の低いネットワークや一時的な切断中に自然に解決することがあります。問題が
-        //                  解決すると、接続は "接続済み "の状態に戻ることがあります。
-        // "failed"       : ICE candidateは、すべての候補のペアを互いにチェックしたが、接続のすべてのコンポーネントに
-        //                  互換性のあるものを見つけることができなかった。しかし、ICEエージェントがいくつかの
-        //                  コンポーネントに対して互換性のある接続を見つけた可能性がある。
-        // see : https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/iceConnectionState
-    };
-
-    // Signaling state change イベントが発生したときのイベントハンドラ
-    // - このイベントは、ピア接続のsignalStateが変化したときに送信される。
-    //   これは、setLocalDescription（）またはsetRemoteDescription（）の呼び出しが原因で発生する可能性がある。
-    //   see : https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/onsignalingstatechange
-    rtcPeerConnection.onsignalingstatechange = () =>
-    {
-        console.log( "Event : Signaling state change" );
-        console.log( "- Signaling state : ", rtcPeerConnection.signalingState );
-    };
-
-    // Connection state change イベントが発生したときのイベントハンドラ
-    // - このイベントは、ピア接続の状態が変化したときに送信される。
-    //   see : https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/onconnectionstatechange
-    rtcPeerConnection.onconnectionstatechange = () =>
-    {
-        console.log( "Event : Connection state change" );
-        console.log( "- Connection state : ", rtcPeerConnection.connectionState );
-        // "disconnected" : 接続のためのICEトランスポートの少なくとも1つが「disconnected」状態であり、
-        //                  他のトランスポートのどれも「failed」、「connecting」、「checking」の状態ではない。
-        // "failed"       : 接続の1つ以上のICEトランスポートが「失敗」状態になっている。
-        // see : https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/connectionState
-    };
-
-    // Track イベントが発生したときのイベントハンドラ
-    // - このイベントは、新しい着信MediaStreamTrackが作成され、
-    //   コネクション上のレシーバーセットに追加されたRTCRtpReceiverオブジェクトに関連付けられたときに送信される。
-    //   see : https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/ontrack
-    // - 古くは、rtcPeerConnection.onaddstream に設定していたが、廃止された。
-    //   現在は、rtcPeerConnection.ontrack に設定する。
-    rtcPeerConnection.ontrack = ( event ) =>
-    {
-        console.log( "Event : Track" );
-        console.log( "- stream", event.streams[0] );
-        console.log( "- track", event.track );
-    };
 }

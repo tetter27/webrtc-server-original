4b79937ebb18c3785f91fd55dec1300650b8e6e1
tetta maeda <tetter@A1003483-tetter.local>
2022-11-11 09:58:29 +0900

    refine

--- eventHandler.js
+++ eventHandler.js
@@ -11,28 +11,28 @@ function setupRTCPeerConnectionEventHandler(rtcPeerConnection){
 
     // Handler for the "ICE Candidate" event
     //   This event always occurs when any ICE agent starts streaming message.
-    rtcPeerConnection.onicecandidate = ( event ) =>
+    rtcPeerConnection.onicecandidate = (event) =>
     {
-        console.log( "Event : ICE candidate" );
-        if( event.candidate )
+        console.log("Event : ICE candidate");
+        if(event.candidate)
         {   // ICE candidate is existed
-            console.log( "- ICE candidate : ", event.candidate );
+            console.log("=> ICE candidate : ", event.candidate);
 
             // If Vanilla ICE, SDP is not sending yet.
             // If Trickle ICE, initial SDP is sending.
         }
         else
-        {   // There are no ICE candiate = Finalize ICE candidate collection.
-            console.log( "- ICE candidate : empty" );
+        {   // There are no ICE candiate => Finalized ICE candidate collection.
+            console.log("=> ICE candidate : empty");
         }
     };
 
     // Handler for the "ICE candidate" error
     //   This event occurs if an error occurs while collecing ICE candidates.
     //   [ToDo] Check https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/onicecandidateerror
-    rtcPeerConnection.onicecandidateerror = ( event ) =>
+    rtcPeerConnection.onicecandidateerror = (event) =>
     {
-        console.error( "Event : ICE candidate error. error code : ", event.errorCode );
+        console.error("Event : ICE candidate error. error code : ", event.errorCode);
     };
 
     // Handler for the "ICE gathering state change" event
@@ -40,16 +40,16 @@ function setupRTCPeerConnectionEventHandler(rtcPeerConnection){
     //   [ToDo] Check https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/onicegatheringstatechange
     rtcPeerConnection.onicegatheringstatechange = () =>
     {
-        console.log( "Event : ICE gathering state change" );
-        console.log( "- ICE gathering state : ", rtcPeerConnection.iceGatheringState );
+        console.log("Event : ICE gathering state change");
+        console.log("=> ICE gathering state : ", rtcPeerConnection.iceGatheringState);
 
-        if( "complete" === rtcPeerConnection.iceGatheringState )
+        if("complete" === rtcPeerConnection.iceGatheringState)
         {
             // If Vanilla ICE, SDP is not sending yet.
             // If Trickle ICE, initial SDP is sending.
 
             // Paseted OfferSDP to textarea
-            console.log( "- Set OfferSDP in textarea" );
+            console.log("=> Set OfferSDP in textarea");
 
             pastelocalSDPtoTextarea(rtcPeerConnection);
 
@@ -64,8 +64,8 @@ function setupRTCPeerConnectionEventHandler(rtcPeerConnection){
     //   [ToDo] Check https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/iceconnectionstatechange_event
     rtcPeerConnection.oniceconnectionstatechange = () =>
     {
-        console.log( "Event : ICE connection state change" );
-        console.log( "- ICE connection state : ", rtcPeerConnection.iceConnectionState );
+        console.log("Event : ICE connection state change");
+        console.log("=> ICE connection state : ", rtcPeerConnection.iceConnectionState);
         // "disconnected" : Failed at least one component to confirm that the component is still connecting.
         //                  This sometimes occurs intermittently, and is solved while temporary disconnection.
         // "failed"       : It means that all candidate peer are confirmed and there are no peer that has compatibility.
@@ -78,8 +78,8 @@ function setupRTCPeerConnectionEventHandler(rtcPeerConnection){
     //   [ToDo] Check https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/onsignalingstatechange
     rtcPeerConnection.onsignalingstatechange = () =>
     {
-        console.log( "Event : Signaling state change" );
-        console.log( "- Signaling state : ", rtcPeerConnection.signalingState );
+        console.log("Event : Signaling state change");
+        console.log("=> Signaling state : ", rtcPeerConnection.signalingState);
     };
 
     // Handler for "Connection state change" event.
@@ -87,8 +87,8 @@ function setupRTCPeerConnectionEventHandler(rtcPeerConnection){
     //   [ToDo] Check https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/onconnectionstatechange
     rtcPeerConnection.onconnectionstatechange = () =>
     {
-        console.log( "Event : Connection state change" );
-        console.log( "- Connection state : ", rtcPeerConnection.connectionState );
+        console.log("Event : Connection state change");
+        console.log("=> Connection state : ", rtcPeerConnection.connectionState);
         // "disconnected" : At least one of the ICE transport are "disconnected" status, and
         //                  all of the other transports are not "failed" or "connecting", "checking" status.
         // "failed"       : At least one of the ICE transport are "failed" status.
@@ -100,10 +100,10 @@ function setupRTCPeerConnectionEventHandler(rtcPeerConnection){
     //   RTCRtpReceiver object that is added to receiver set on the connection.
     //   [ToDo] Check https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/ontrack
     //   !!! rtcPeerConnection.onaddstream is duplicated.
-    rtcPeerConnection.ontrack = ( event ) =>
+    rtcPeerConnection.ontrack = (event) =>
     {
-        console.log( "Event : Track" );
-        console.log( "- stream", event.streams[0] );
-        console.log( "- track", event.track );
+        console.log("Event : Track");
+        console.log("=> stream", event.streams[0]);
+        console.log("=> track", event.track);
     };
 }
--- userMedia.js
+++ userMedia.js
@@ -1,45 +1,74 @@
 "use strict";
 
-const elementVideoLocal = document.getElementById( "local_video_box" );
-const elementTextareaLocalSDP = document.getElementById( "local_sdp_text" );
-const elementTextareaRemoteSDP = document.getElementById( "remote_sdp_text" );
+const elementVideoLocal = document.getElementById("local_video_box");
+const elementTextPageRole = document.getElementById("page_role_text");
+const elementButtonSetOfferer = document.getElementById("set_offerer_button");
+const elementButtonSetAnswerer = document.getElementById("set_answerer_button");
+const elementTextareaLocalSDP = document.getElementById("local_sdp_text");
+const elementTextareaRemoteSDP = document.getElementById("remote_sdp_text");
 
+let pageRole = null;
+let localStream = null;
+let remoteStream = null;
 
 // Run when just accessed this page.
 navigator.mediaDevices.getUserMedia({"video": true, /*"audio": true*/})
-.then( ( stream ) =>
+.then((stream) =>
 {
     //Display video from usb camera as the local stream.
-    elementVideoLocal.srcObject = stream;
+    localStream = stream
+    elementVideoLocal.srcObject = localStream;
     console.log("Stream has set.");
 
-    let config = { "iceServers": [] };
-    let rtcPeerConnection = new RTCPeerConnection( config );
+})
+.catch((error) =>
+{
+    console.error("Error : ", error);
+    alert("Could not setup.");
+    return;
+});
+
+
+
+function onclickButton_SetAsOfferer(){
+
+    console.log("Selected Offerer.");
+    elementTextPageRole.innerHTML = "You are Offerer!";
+
+    pageRole = "Offerer";
+    elementButtonSetOfferer.style.visibility = "hidden";
+    elementButtonSetAnswerer.style.visibility = "hidden";
+
+    let config = {"iceServers": []};
+    let rtcPeerConnection = new RTCPeerConnection(config);
 
     // The OfferSDP is pasted when "ICE gathering state change" becomes "complete".
-    setupRTCPeerConnectionEventHandler( rtcPeerConnection );
+    setupRTCPeerConnectionEventHandler(rtcPeerConnection);
 
-    stream.getTracks().forEach( ( track ) =>
+    localStream.getTracks().forEach((track) =>
         {
-            rtcPeerConnection.addTrack( track, stream );
-        } );
+            rtcPeerConnection.addTrack(track, localStream);
+        });
 
     // Create OfferSDP
-    createOfferSDP( rtcPeerConnection );
-} )
-.catch( ( error ) =>
-{
-    console.error( "Error : ", error );
-    alert( "Could not setup." );
-    return;
-} );
+    createOfferSDP(rtcPeerConnection);
+}
 
 
+function onclickButton_SetAsAnswerer(){
+
+    console.log("Selected Answerer.");
+    elementTextPageRole.innerHTML = "You are Answerer!";
+
+    pageRole = "Answerer";
+    elementButtonSetOfferer.style.visibility = "hidden";
+    elementButtonSetAnswerer.style.visibility = "hidden";
+}
 
 
 function createPeerConnection(stream){
     let config = {"iceServer":[]};
-    let rtcPeerConnection = new RTCPeerConnection( config );
+    let rtcPeerConnection = new RTCPeerConnection(config);
 
     // Event handler for RTCPeetConnection
     setupRTCPeerConnectionEventHandler(rtcPeerConnection);
@@ -60,23 +89,23 @@ function createPeerConnection(stream){
 function createOfferSDP(rtcPeerConnection)
 {
 
-    console.log( "Call : rtcPeerConnection.createOffer()" );
+    console.log("Call : rtcPeerConnection.createOffer()");
     rtcPeerConnection.createOffer()
-        .then( ( sessionDescription ) =>
+        .then((sessionDescription) =>
         {
             // Set OfferSDP ot LocalDescription
-            console.log( "Call : rtcPeerConnection.setLocalDescription()" );
-            return rtcPeerConnection.setLocalDescription( sessionDescription );
-        } )
-        .then( () =>
+            console.log("Call : rtcPeerConnection.setLocalDescription()");
+            return rtcPeerConnection.setLocalDescription(sessionDescription);
+        })
+        .then(() =>
         {
             // If Vanilla ICE, SDP is not sending yet.
             // If Trickle ICE, initial SDP is sending.
-        } )
-        .catch( ( error ) =>
+        })
+        .catch((error) =>
         {
-            console.error( "Error : ", error );
-        } );
+            console.error("Error : ", error);
+        });
 }
 
 // The offer SDP is pasted when "ICE gathering state change" becomes "complete".
@@ -85,3 +114,6 @@ function pastelocalSDPtoTextarea(rtcPeerConnection){
     elementTextareaLocalSDP.value = rtcPeerConnection.localDescription.sdp;
 
 }
+
+
+
